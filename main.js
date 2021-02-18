317a90f7626fed249f94a4454b93456420c1985d
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-02-18 16:12:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ Webcam.set({
 });
 var camera=document.getElementById("camera");
 Webcam.attach(camera);
+function take_p(){
+ Webcam.snap(function(data_uri){
+     document.getElementById("Photo").innerHTML= "<img id='Snapshot' src ='"+data_uri+"'>";
+ });
+};
+console.log(ml5.version);
+ classifier = ml5.imageClassifier("MobileNet",modelLoded);
+function modelLoded(){
+    console.log("Model is loaded.");
+}
+function iden(){
+    Photo= document.getElementById("Snapshot");
+    classifier.classify(Photo,gotResult);
+};
+function gotResult(error,result){
+if(error){
+    console.log(error);
+}
+else if(result){
+    console.log(result);
+    document.getElementById("object-label").innerHTML = result[0].label;
+}
+};
